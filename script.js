34b6fac78fff976529a10ce177d3c6c45e37993f
Leandro Castro <leandrocastrold@hotmail.com>
2020-09-15 22:33:51 -0300

    Objects Snake and food created

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
  const stage = document.querySelector('#canvasStage'); 
  const ctx = stage.getContext('2d');
  const resolutionSelect = document.querySelector('#resolutionSet');
+ 
+const FPS = 60;
+
+const snake = {
+    headSize: 200,
+    draw: function() {
+        ctx.fillStyle = "red";
+        ctx.fillRect(0, 0, 15, 15);
+        console.log("Desenhou CObra" + this.headSize)
+    }
+}
+
+const food = {
+    size: 80,
+        posX: Math.random() * 200,
+        posY: Math.random() * 200,
+    draw: function () {
+        ctx.fillStyle = "green";
+        ctx.fillRect(this.posX, this.posY, this.size, this.size);
+        console.log("Desenhou CObra" + this.headSize)
+    }
+}
 
 const setStageResolution = () => {
     let values = resolutionSelect.value.split('x');
        stage.width = parseInt(values[0]),
        stage.height = parseInt(values[1])
-    console.log(ctx);
 }
 
 const createStage = () => {
@@ -19,6 +40,8 @@ const redrawScreen = () => {
     createStage();
 }
 
+
+
 resolutionSelect.addEventListener("change", () => {
     setStageResolution();
     redrawScreen();
@@ -27,3 +50,6 @@ resolutionSelect.addEventListener("change", () => {
 
 setStageResolution();
 createStage();
+snake.draw();
+food.draw();
+
