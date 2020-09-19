0f0f99652bbb54da2fed15a969b17474d73d30f5
Leandro Castro <leandrocastrold@hotmail.com>
2020-09-18 21:08:21 -0300

    Game over screen and collision detection added

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ const stage = document.querySelector('#canvasStage');
 const ctx = stage.getContext('2d');
 const resolutionSelect = document.querySelector('#resolutionSet');
 
-const pixelQuantity = stage.width/ 20;
-let pixelSize = 0; 
+let pixelQuantity = 0;
+let pixelSize = 0;
 
-const FPS = 60;
+const FPS = 250;
+
+const food = {
+    posX: 0,
+    posY: 0,
+    draw: function () {
+        ctx.fillStyle = "green";
+        ctx.fillRect(this.posX * pixelSize, this.posY * pixelSize, pixelSize, pixelSize);
+    },
+}
 
 const snake = {
     speed: 1,
@@ -20,26 +29,19 @@ const snake = {
     },
 }
 
-const food = {
-    posX: 0,
-    posY: 0,
-    draw: function () {
-        ctx.fillStyle = "green";
-        ctx.fillRect(this.posX, this.posY, pixelSize, pixelSize);
-    },
-
-}
 
 const changeFoodPosition = () => {
-    food.posX = Math.floor(Math.random() * pixelSize),
-    food.posY = Math.floor(Math.random() * pixelSize)
+    food.posX = Math.floor(Math.random() * (pixelQuantity - 1)),
+        food.posY = Math.floor(Math.random() * (stage.height / pixelSize - 1))
+        console.log(`Food position: posX ${food.posX} posY ${food.posY}`)
 }
 
 const setStageResolution = () => {
     let values = resolutionSelect.value.split('x');
     stage.width = parseInt(values[0]),
         stage.height = parseInt(values[1])
-    pixelSize = stage.width / 40;
+    pixelSize = stage.width / 20;
+    pixelQuantity = stage.width / pixelSize - 1;
 }
 
 const createStage = () => {
@@ -57,59 +59,81 @@ const checkCollision = () => {
     if (snake.posX == food.posX && snake.posY == food.posY) {
         changeFoodPosition();
         snake.score += 10;
-    }
+    } else if (snake.posX > pixelQuantity || snake.posX < 0 || snake.posY > stage.height / pixelSize - 1 || snake.posY < 0) {
+        console.log("Saiu da tela")
+        snake.velX = 0;
+        snake.velY = 0;
+        clearInterval(interval)
+        showGameOver();
+    } 
 }
 
-    const game = () => {
-        checkCollision();
-        updateScreen();
-        snake.draw();
-        food.draw();
-    }
+const game = () => {
+    console.log('rodando')
+    updateScreen();
+    snake.draw();
+    food.draw();
+    checkCollision();
+}
 
-    const updateScreen = () => {
-        ctx.clearRect(0, 0, stage.width, stage.height)
-        createStage();
-        showScore();
-    }
+const updateScreen = () => {
+    ctx.clearRect(0, 0, stage.width, stage.height)
+    createStage();
+    showScore();
+}
 
 
-    resolutionSelect.addEventListener("change", () => {
-        setStageResolution();
-        updateScreen();
+resolutionSelect.addEventListener("change", () => {
+    setStageResolution();
+    updateScreen();
 
-    })
+})
 
-    document.addEventListener("keydown", evento => {
-        const keyPressed = evento.key;
-        switch (keyPressed) {
-            case "ArrowUp":
-                console.log("Subindo...");
+const showGameOver = () => {
+    ctx.clearRect(0, 0, stage.width, stage.height)
+    createStage();
+    ctx.fillStyle = "white";
+    ctx.font = "50px Arial bold"
+    ctx.fillText("GAME OVER", stage.width / 4, stage.height / 3);
+    ctx.fillText(`Sua pontuação: ${snake.score} `, stage.width / 6, stage.height / 2);
+    let button = document.createElement("button")
+    stage.appendChild(button);
+}
+
+document.addEventListener("keydown", evento => {
+    const keyPressed = evento.key;
+    switch (keyPressed) {
+        case "ArrowUp":
+            if (snake.velY == 0) {
                 snake.velX = 0;
                 snake.velY = -snake.speed;
-                break;
+            }
+            break;
 
-            case "ArrowRight":
-                console.log("Descendo...");
+        case "ArrowRight":
+            if (snake.velX == 0) {
                 snake.velX = snake.speed;
                 snake.velY = 0;
-                break;
+            }
+            break;
 
-            case "ArrowDown":
-                console.log("Descendo...");
+        case "ArrowDown":
+            if (snake.velY == 0) {
                 snake.velX = 0;
                 snake.velY = snake.speed;
-                break;
+            }
+            break;
 
-            case "ArrowLeft":
-                console.log("Descendo...");
+        case "ArrowLeft":
+            if (snake.velX == 0) {
                 snake.velX = -snake.speed;
                 snake.velY = 0;
-                break;
-        }
-    })
+            }
+            break;
+    }
+})
 
 setStageResolution();
 createStage();
 showScore();
-setInterval(game, FPS)
+let interval = setInterval(game, FPS)
