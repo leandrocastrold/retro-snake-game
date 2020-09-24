8b3a98d06fa782503637f6221ce899db4f8df7c8
Leandro Castro <leandrocastrold@hotmail.com>
2020-09-23 22:53:52 -0300

    Endless stage added

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const stage = document.querySelector('#canvasStage');
 const ctx = stage.getContext('2d');
 const resolutionSelect = document.querySelector('#resolutionSet');
-
+let pixelQuantity;
 const pixelSize = 20;
 let direction = "";
 
 let snake = []
 
 snake[0] = {
-    x: 15 * pixelSize,
+    x: 0 * pixelSize,
     y: 15 * pixelSize,
 }
 
@@ -27,25 +27,24 @@ const drawFood = () => {
 const randomizeFoodPosition = () => {
     valueX = Math.floor(Math.random() * 15 + 1) * pixelSize;
     valueY = Math.floor(Math.random() * 15 + 1) * pixelSize;
-    
+
     if (!snake.some(item => item.x == valueX && item.y == valueY)) {
         food.x = valueX;
         food.y = valueY;
     } else {
         randomizeFoodPosition();
     }
-
-
 }
 
 const createBackground = () => {
-    stage.width = 20 * pixelSize;
-    stage.height = 20 * pixelSize;
+    pixelQuantity = 20 * pixelSize;
+    stage.width = pixelQuantity;
+    stage.height = pixelQuantity;
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, stage.width, stage.height);
 }
 
-const createSnake = () => {
+const drawSnake = () => {
     for (i = 0; i < snake.length; i++) {
         ctx.fillStyle = "green";
         ctx.fillRect(snake[i].x, snake[i].y, pixelSize, pixelSize);
@@ -97,10 +96,23 @@ const setSnakeMovement = () => {
     }
 }
 
+const checkSnakePosition = () => {
+    if (snake[0].x >= pixelQuantity) {
+       snake[0].x = 0
+    } else if (snake[0].x < 0) {
+        snake[0].x = pixelQuantity - 20 ;
+    }
+    if (snake[0].y >= pixelQuantity) {
+        snake[0].y = 0
+     } else if (snake[0].y < 0) {
+         snake[0].y = pixelQuantity - 20;
+     }
+}
+
 const checkSnakeCollision = () => {
 
-    for(i = 1; i < snake.length; i++){
-        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
             gameOver();
         }
     }
@@ -125,22 +137,21 @@ const checkSnakeCollision = () => {
 const gameOver = () => {
     //clearInterval(jogo);
     direction = ""
-    for (i = snake.length; i > 0; i--){
+    for (i = snake.length; i > 0; i--) {
         snake.pop();
         console.log("Snake Length: " + snake.length)
         createBackground();
         createSnake();
     }
-    
+
     alert('Game Over :(');
-   
 }
-
-
 
 const update = () => {
     createBackground();
-    createSnake();
+    checkSnakePosition();
+    drawSnake();
+    
     checkSnakeCollision();
     drawFood();
     setSnakeMovement();
@@ -148,4 +159,4 @@ const update = () => {
 
 
 document.addEventListener("keydown", snakeControls);
-let jogo = setInterval(update, 80);
+let jogo = setInterval(update, 250);
